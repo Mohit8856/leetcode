aecb0bf6fc8f1d6eab97a83df156f028a9b03075
Mohit Yadav <my8549228@gmail.com>
2026-02-19 13:58:19 +0530

    Update 72.py

--- 72.py
+++ 72.py
@@ -1,15 +1,22 @@
-word1 = "horse"
-word2 = "ros"
+def minDistance(self, word1, word2):
+        m, n = len(word1), len(word2)
 
-count = 0
-min_len = min(len(word1), len(word2))
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-for i in range(min_len):
-    if word1[i] != word2[i]:
-        count += 1
-    elif i > 0 and word1[i] == word2[i-1]:
-        count -= 1
+        for i in range(m + 1):
+           dp[i][0] = i
+        for j in range(n + 1):
+           dp[0][j] = j
 
-count += abs(len(word1) - len(word2))
+        for i in range(1, m + 1):
+             for j in range(1, n + 1):
+                 if word1[i - 1] == word2[j - 1]:
+                     dp[i][j] = dp[i - 1][j - 1]
+                 else:
+                     dp[i][j] = 1 + min(
+                     dp[i - 1][j],     
+                     dp[i][j - 1],     
+                     dp[i - 1][j - 1] 
+            )
 
-print(count)
+        return (dp[m][n])
