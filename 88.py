ed02a235f3e3db8dd07fa3457370c42730c5167f
Mohit Yadav <my8549228@gmail.com>
2026-02-22 18:55:50 +0530

    Update 88.py

--- 88.py
+++ 88.py
@@ -1,7 +1,9 @@
-newnums1 = nums1[:m]
-newnums2 = nums2[:n]
+# to run this code in vs code just change the last line to print and if you want to use it directly in leetcode so you can just ctrl-c to ctrl-v
+newnums1 = nums1[:m] # indexing
+newnums2 = nums2[:n] # indexing
 
-newnums = newnums1 + newnums2
-newnums.sort()
+newnums = newnums1 + newnums2 # addingf to indexed lists
+newnums.sort() # sorting
 
-nums1[:] = newnums  
+
+nums1[:] = newnums  # updating nums1 not returning it as per the question
