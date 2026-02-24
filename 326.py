d2540b39725ae239d3a9d8fda311653833c2ec7a
Mohit Yadav <my8549228@gmail.com>
2026-02-24 16:48:21 +0530

    Update 326.py

--- 326.py
+++ 326.py
@@ -7,6 +7,8 @@ while 3**i <= n: #condition
         break # break this block  of code 
     else:
         i += 1 # update the value of i to cheack further
-else: # if above condition in while loop will  satisfy then  it will five , print or return false as default  
+else: # if above condition in while loop will  satisfy then  it will five , print or return false as default   but if condition  inside the while loop is satisfy then it break and no exsicution take place
 
     print("False")
+
+# to use this code in vs code you can directly do ctrl-c to ctrl-v  but if you want it on leetcode then replace print with return and remove n = 27
